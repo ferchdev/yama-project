29c035e45903129cc216e9915d002297fa49767c
Fernando Soto <fernandosotto5888@gmail.com>
2023-06-09 20:25:07 -0400

    update

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,7 +1,9 @@
+import banner from "../assets/banners/banner.png";
+
 export const HomeBanner = () => {
   return (
     <section className="HomeBanner">
-      <section className="principal-banner w-[100%] h-[100vh] bg-principal-banner bg-center bg-cover relative">
+      <section className={`principal-banner w-[100%] h-[100vh] bg-[url(${banner})] bg-center bg-cover relative`}>
         <div className="w-[95%] mx-auto sm:ps-4">
           <h1
             className="principal-banner__h1 text-white text-[35px] text-start sm:text-[55px] font-semibold w-[80%] sm:w-[480px] absolute bottom-[35vh] tracking-widest"
