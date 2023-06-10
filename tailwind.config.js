1803e05be04f40cc983099676c636dd8d195119f
Fernando Soto <fernandosotto5888@gmail.com>
2023-06-09 20:29:52 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#191f2b',
-        'bordercolor' : '#e08637'
+        primary: "#191f2b",
+        bordercolor: "#e08637",
       },
       backgroundImage: {
-        'nav' : 'linear-gradient(#0000000e, #e08637, #e08637, #e08637,#0000000e);',
-        'principal-banner' : "url('./src/assets/banners/banner.png')"
+        nav: "linear-gradient(#0000000e, #e08637, #e08637, #e08637,#0000000e);",
+        "principal-banner": "url('/src/assets/banners/banner.png')",
       },
     },
   },
